45a055869f630204ae3d9c2e6fdefc7e3d8389c1
solarion83 <65498066+solarion83@users.noreply.github.com>
2020-05-22 01:45:13 +0200

    Major revamp
    Switched to event listeners
    made dropdown dynamic
    added hauptphaseverrechnen
    added preAttack
    added attack

--- js/gamemanager.js
+++ js/gamemanager.js
@@ -1,46 +1,38 @@
 const getInterface = document.querySelector(".interface");
+const getInstruction = document.querySelector(".instruction");
+const displayPlayerPoints = document.querySelector(".playerPoints");
+const displayOpponentPoints = document.querySelector(".opponentPoints");
 const getDiceArea = document.querySelector(".dicearea");
 const getInteractionArea = document.querySelector(".interactionarea");
 
-var dice1 = new Dice;
-var dice2 = new Dice;
-var dice3 = new Dice;
-var dice4 = new Dice;
-var dice5 = new Dice;
-var dice6 = new Dice;
-
 var unselectedDice = new Array;
-
-/*
-for (i = 0; i < 6; i++) {
-	let x = i + 1;
-	unselectedDice[i] = dice + x;
-}
-*/
-
-unselectedDice[0] = dice1;
-unselectedDice[1] = dice2;
-unselectedDice[2] = dice3;
-unselectedDice[3] = dice4;
-unselectedDice[4] = dice5;
-unselectedDice[5] = dice6;
 
 let sumDice;
 
-function getSumDice() {
-	for	(i=0;i<unselectedDice.length;i++) {
-		sumDice += unselectedDice[i].lastRoll;
-
-	return sumDice;
-	}
-}
-	
+let playerPoints = 0;
+let opponentPoints = 0;
 
 var GameManager = {
 
 	initGame: function() {
 
+		//Würfel zurücksetzen
+		var dice1 = new Dice;
+		var dice2 = new Dice;
+		var dice3 = new Dice;
+		var dice4 = new Dice;
+		var dice5 = new Dice;
+		var dice6 = new Dice;
+		unselectedDice[0] = dice1;
+		unselectedDice[1] = dice2;
+		unselectedDice[2] = dice3;
+		unselectedDice[3] = dice4;
+		unselectedDice[4] = dice5;
+		unselectedDice[5] = dice6;
+
+		getDiceArea.innerHTML = "";
 		getInteractionArea.innerHTML = "<button class=\"btn-wuerfeln\" onclick=\"GameManager.throwDice()\">Würfeln!</button>";
+
 	},
 
 
@@ -54,69 +46,123 @@ var GameManager = {
 			}
 		getDiceArea.innerHTML = getDiceArea.innerHTML + unselectedDice[i].imgPath;
 		}
-
-		getSumDice();
 		
-		console.log(getSumDice());
+		console.log(GameManager.getSumDice());
 
-		getInteractionArea.innerHTML = " <label>Welchen Würfel möchtest du behalten?<label><select id=\"eingabebehalten\" multiple><option value=\"1\">Würfel 1</option><option value=\"2\">Würfel 2</option><option value=\"3\">Würfel 3</option><option value=\"4\">Würfel 4</option><option value=\"5\">Würfel 5</option><option value=\"6\">Würfel 6</option></select><button onclick=\"GameManager.behalten()\">Ok</button>";
+		let dropdownOptions;
+
+		for (i = 0; i < unselectedDice.length; i++) {
+			if	(!unselectedDice[i].getIsPicked()) {
+				dropdownOptions += "<option value=\""+(i+1)+"\">Würfel "+(i+1)+"</option>";
+			}
+		}
+
+		getInteractionArea.innerHTML = " <label>Welchen Würfel möchtest du behalten?</label><select id=\"eingabebehalten\" multiple>"+dropdownOptions+"</select><button class=\"btn-behalten\">Ok</button><button class=\"btn-aufhoeren\">Aufhören</button>";
+
+		$(".btn-behalten").click(function () {
+			
+			GameManager.behalten();
+		})
+
+		$(".btn-aufhoeren").click(function () {
+  
+			GameManager.hauptPhaseVerrechnen();
+			return;
+		})
 	},
 
 	behalten: function() {
+		
 		var getBehaltenValue = $("#eingabebehalten").val();
 
-		
-		
 		console.log(getBehaltenValue);
+
+		if (getBehaltenValue == "") {
+			alert("Du musst mindestens einen Würfel wählen!");
+			return;
+		}
 
 		for (j=0; j < getBehaltenValue.length; j++) {
 			for (i=0; i < unselectedDice.length; i++) {
 				if ((i+1)==getBehaltenValue[j]) {
 					unselectedDice[i].setIsPicked();
 				}
-	
-				
-				/* 
-				//Debug
-				if (unselectedDice[i].getIsPicked()==true) {
-					console.log("true");
-				} else {
-					console.log("false");
-				} 
-				*/
 			}
 		}
 
-		//this.throwDice();
-		getInteractionArea.innerHTML = "Behaltene Würfel: " + getBehaltenValue + " <button onlick=\"GameManager.throwDice()\">Weiterwürfeln</button>";
-			
+		if ((unselectedDice.find(x => x.isPicked === false)==null)) {
+			console.log("Alle Würfel gewählt. Weiter gehts mit der nächsten Funktion!");
+			GameManager.hauptPhaseVerrechnen();
+			return;
 
-		/*if (getBehaltenValue === null && getBehaltenValue === "") {
-			
-			alert("Bitte gib eine Zahl ein. Das Feld darf nicht leer sein!");
-			
-		} else if (getBehaltenValue <1 || getBehaltenValue >6) {
+		}
 
-			alert("Bitte gib eine Zahl zwischen 1 und 6 ein.");
+		getInteractionArea.innerHTML = "Behaltene Würfel: " + getBehaltenValue + " <button class=\"btn-weiterwuerfeln\">Weiterwürfeln</button><button class=\"btn-aufhoeren\">Aufhören</button>";
 
-		} else {
-			
-			for (i=0; i < unselectedDice.length; i++) {
-				if ((i+1)==getBehaltenValue) {
-					unselectedDice[i].setIsPicked();
-				}
-				if (unselectedDice[i].getIsPicked()==true) {
-					console.log("true");
-				} else {
-					console.log("false");
-				}
-			}
-			
+		$(".btn-weiterwuerfeln").click(function () {
+  
+			GameManager.throwDice();
+			return;
+		})
+
+		$(".btn-aufhoeren").click(function () {
+  
+			GameManager.hauptPhaseVerrechnen();
+			return;
+		})
 
 			
-		}*/
+	},
+
+	hauptPhaseVerrechnen: function() {
+
+		if (GameManager.getSumDice() < 30) {
+			
+			playerPoints += (30 - GameManager.getSumDice());
+			displayPlayerPoints.innerHTML = "Punkte Spieler 1:<br>"+playerPoints+"</div>";
+			GameManager.initGame();
+
+		} else if (GameManager.getSumDice() == 30) {
+			GameManager.initGame();
+		} else if (GameManager.getSumDice() > 30) {
+			GameManager.preAttack();
+		}
+	},
+
+	preAttack: function() {
+
+		getInstruction.textContent = "Auf den Gegner würfeln!";
+
+		// Würfel zurücksetzen 
+		var dice1 = new Dice;
+		var dice2 = new Dice;
+		var dice3 = new Dice;
+		var dice4 = new Dice;
+		var dice5 = new Dice;
+		var dice6 = new Dice;
+		unselectedDice[0] = dice1;
+		unselectedDice[1] = dice2;
+		unselectedDice[2] = dice3;
+		unselectedDice[3] = dice4;
+		unselectedDice[4] = dice5;
+		unselectedDice[5] = dice6;
+
+		getDiceArea.innerHTML = "";
+
+		GameManager.attack();
+
+
+
+	},
+
+	attack: function() {
+		
+
+	},
+
+	getSumDice: function() {
+		
+		return unselectedDice.reduce((a, {lastRoll}) => a + lastRoll, 0);
 	}
 
 }
-
-//test
