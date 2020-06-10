9f3011bdda126ec931104d13c5901c1e19d800b0
Michael Bayer <michael.bayer@arsmedium.com>
2020-06-10 22:56:50 +0200

    chore: example data structure

---
+++ js/structure.js
@@ -0,0 +1,418 @@
+const gameSequences = [
+  [
+    {
+      type: 'scoring',
+      player: 1,
+      roundes: [
+        {
+          dices: [
+            {
+              value: 1,
+              picked: false,
+            },
+            {
+              value: 1,
+              picked: false,
+            },
+            {
+              value: 1,
+              picked: false,
+            },
+            {
+              value: 6,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            }
+          ]
+        },
+        {
+          dices: [
+            {
+              value: 5,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            }
+          ]
+        },
+      ]
+    },
+    {
+      type: 'attack',
+      roundes: [
+        {
+          player: 1,
+          type: 'attack',
+          dices: [
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+          ]
+        },
+        {
+          player: 2,
+          type: 'attack',
+          dices: [
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+          ]
+        }
+      ]
+    },
+    {
+      type: 'scoring',
+      player: 2,
+      roundes: [
+        {
+          dices: [
+            {
+              value: 1,
+              picked: false,
+            },
+            {
+              value: 1,
+              picked: false,
+            },
+            {
+              value: 1,
+              picked: false,
+            },
+            {
+              value: 6,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            }
+          ]
+        },
+        {
+          dices: [
+            {
+              value: 5,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            }
+          ]
+        },
+      ]
+    },
+    {
+      type: 'attack',
+      roundes: [
+        {
+          player: 2,
+          type: 'attack',
+          dices: [
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+          ]
+        },
+        {
+          player: 1,
+          type: 'attack',
+          dices: [
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+          ]
+        }
+      ]
+    }
+  ],
+  [
+    {
+      type: 'scoring',
+      player: 1,
+      roundes: [
+        {
+          dices: [
+            {
+              value: 1,
+              picked: false,
+            },
+            {
+              value: 1,
+              picked: false,
+            },
+            {
+              value: 1,
+              picked: false,
+            },
+            {
+              value: 6,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            }
+          ]
+        },
+        {
+          dices: [
+            {
+              value: 5,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            }
+          ]
+        },
+      ]
+    },
+    {
+      type: 'attack',
+      roundes: [
+        {
+          player: 1,
+          type: 'attack',
+          dices: [
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+          ]
+        },
+        {
+          player: 2,
+          type: 'attack',
+          dices: [
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+          ]
+        }
+      ]
+    },
+    {
+      type: 'scoring',
+      player: 2,
+      roundes: [
+        {
+          dices: [
+            {
+              value: 1,
+              picked: false,
+            },
+            {
+              value: 1,
+              picked: false,
+            },
+            {
+              value: 1,
+              picked: false,
+            },
+            {
+              value: 6,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            }
+          ]
+        },
+        {
+          dices: [
+            {
+              value: 5,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            },
+            {
+              value: 6,
+              picked: true,
+            }
+          ]
+        },
+      ]
+    },
+    {
+      type: 'attack',
+      roundes: [
+        {
+          player: 2,
+          type: 'attack',
+          dices: [
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+            {
+              value: 1
+            },
+          ]
+        },
+        {
+          player: 1,
+          type: 'attack',
+          dices: [
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+            {
+              value: 5
+            },
+          ]
+        }
+      ]
+    }
+  ]
+]
